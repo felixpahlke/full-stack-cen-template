4768ec47c3ad160d75b575475eb22c011ee92626
felixpahlke <felix.pahlke@ibm.com>
2024-11-15 11:57:36 +0100

    chore: add swagger ui persist auth

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
+    swagger_ui_parameters={"persistAuthorization": True},
 )
 
 
