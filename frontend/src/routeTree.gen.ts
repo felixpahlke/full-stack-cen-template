33c4cfdd776404ffe525d9d858554090a150943d
felixpahlke <felix.pahlke@ibm.com>
2024-12-04 14:34:05 +0100

    chore: formatting & linting scripts

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -10,84 +10,84 @@
 
 // Import Routes
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as SignupImport } from "./routes/signup";
-import { Route as LoginImport } from "./routes/login";
-import { Route as LayoutImport } from "./routes/_layout";
-import { Route as LayoutIndexImport } from "./routes/_layout/index";
-import { Route as LayoutSettingsImport } from "./routes/_layout/settings";
-import { Route as LayoutItemsImport } from "./routes/_layout/items";
-import { Route as LayoutAdminImport } from "./routes/_layout/admin";
+import { Route as rootRoute } from './routes/__root'
+import { Route as SignupImport } from './routes/signup'
+import { Route as LoginImport } from './routes/login'
+import { Route as LayoutImport } from './routes/_layout'
+import { Route as LayoutIndexImport } from './routes/_layout/index'
+import { Route as LayoutSettingsImport } from './routes/_layout/settings'
+import { Route as LayoutItemsImport } from './routes/_layout/items'
+import { Route as LayoutAdminImport } from './routes/_layout/admin'
 
 // Create/Update Routes
 
 const SignupRoute = SignupImport.update({
-  path: "/signup",
+  path: '/signup',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const LoginRoute = LoginImport.update({
-  path: "/login",
+  path: '/login',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const LayoutRoute = LayoutImport.update({
-  id: "/_layout",
+  id: '/_layout',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const LayoutIndexRoute = LayoutIndexImport.update({
-  path: "/",
+  path: '/',
   getParentRoute: () => LayoutRoute,
-} as any);
+} as any)
 
 const LayoutSettingsRoute = LayoutSettingsImport.update({
-  path: "/settings",
+  path: '/settings',
   getParentRoute: () => LayoutRoute,
-} as any);
+} as any)
 
 const LayoutItemsRoute = LayoutItemsImport.update({
-  path: "/items",
+  path: '/items',
   getParentRoute: () => LayoutRoute,
-} as any);
+} as any)
 
 const LayoutAdminRoute = LayoutAdminImport.update({
-  path: "/admin",
+  path: '/admin',
   getParentRoute: () => LayoutRoute,
-} as any);
+} as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/_layout": {
-      preLoaderRoute: typeof LayoutImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/login": {
-      preLoaderRoute: typeof LoginImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/signup": {
-      preLoaderRoute: typeof SignupImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/_layout/admin": {
-      preLoaderRoute: typeof LayoutAdminImport;
-      parentRoute: typeof LayoutImport;
-    };
-    "/_layout/items": {
-      preLoaderRoute: typeof LayoutItemsImport;
-      parentRoute: typeof LayoutImport;
-    };
-    "/_layout/settings": {
-      preLoaderRoute: typeof LayoutSettingsImport;
-      parentRoute: typeof LayoutImport;
-    };
-    "/_layout/": {
-      preLoaderRoute: typeof LayoutIndexImport;
-      parentRoute: typeof LayoutImport;
-    };
+    '/_layout': {
+      preLoaderRoute: typeof LayoutImport
+      parentRoute: typeof rootRoute
+    }
+    '/login': {
+      preLoaderRoute: typeof LoginImport
+      parentRoute: typeof rootRoute
+    }
+    '/signup': {
+      preLoaderRoute: typeof SignupImport
+      parentRoute: typeof rootRoute
+    }
+    '/_layout/admin': {
+      preLoaderRoute: typeof LayoutAdminImport
+      parentRoute: typeof LayoutImport
+    }
+    '/_layout/items': {
+      preLoaderRoute: typeof LayoutItemsImport
+      parentRoute: typeof LayoutImport
+    }
+    '/_layout/settings': {
+      preLoaderRoute: typeof LayoutSettingsImport
+      parentRoute: typeof LayoutImport
+    }
+    '/_layout/': {
+      preLoaderRoute: typeof LayoutIndexImport
+      parentRoute: typeof LayoutImport
+    }
   }
 }
 
@@ -102,6 +102,6 @@ export const routeTree = rootRoute.addChildren([
   ]),
   LoginRoute,
   SignupRoute,
-]);
+])
 
 /* prettier-ignore-end */
