f77cfaadf262cd5e7ddfb0e994b07d4e5f43e3b8
felixpahlke <felix.pahlke@ibm.com>
2025-02-05 17:10:45 +0100

    modified generate client

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -3,9 +3,12 @@
 set -e
 set -x
 
-cd backend
-python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
-cd ..
+# cd backend
+# python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
+# cd ..
+
+yq -o json backend/docs/openapi.yaml > ./openapi.json
+
 node frontend/modify-openapi-operationids.js
 mv openapi.json frontend/
 cd frontend
