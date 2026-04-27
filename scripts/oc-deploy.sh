279a01b3b37dd83d697fd1da163436ee3e3d0c68
Kilian Popp <kilian.popp@ibm.com>
2026-04-27 17:00:04 +0200

    openshift deployment: if registry is not working, it tries to fix it, but since there are sooo many options to configure it only handles the main issues

--- scripts/oc-deploy.sh
+++ scripts/oc-deploy.sh
@@ -170,6 +170,9 @@ main() {
         exit 0
     fi
     
+    # Add Openshift registry if missing
+    setup_image_registry || exit 1
+
     # ============================================================
     # PHASE 4: DATABASE DEPLOYMENT
     # ============================================================
